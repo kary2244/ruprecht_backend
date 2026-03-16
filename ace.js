f5abc8a8906feb87d94d6bd96392d91cff39d42a
kary2244 <karyrey52@gmail.com>
2026-03-16 13:20:21 -0600

    Comment out ts-node import in ace.js

--- ace.js
+++ ace.js
@@ -19,7 +19,7 @@
 /**
  * Register hook to process TypeScript files using ts-node
  */
-import 'ts-node-maintained/register/esm'
+//import 'ts-node-maintained/register/esm'
 
 /**
  * Import ace console entrypoint
